a9bfb13c7e5af036dd8cf2f192b8527ce23124ed
dbdmsdn10 <dbdmsdn10@gmail.com>
2021-04-30 14:11:37 +0900

    자동로그인 기능 구현
    현 계정이 아닌 새로운 계정에 적용됨,
    
    요청시 현계정에도 적용시킬수도 있음
    로그아웃기능없어서 로그인 기능 확인하고 싶다면 앱을 지워야함

---
+++ app/src/main/java/com/example/schoolfinalproject/MypageInfo.java
@@ -0,0 +1,31 @@
+package com.example.schoolfinalproject;
+
+public class MypageInfo {
+    String auto;
+    String gender;
+    String name;
+
+    public String getAuto() {
+        return auto;
+    }
+
+    public void setAuto(String auto) {
+        this.auto = auto;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
